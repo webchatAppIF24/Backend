02e84630d6f5d6cccb17579d4df664b3480a108b
gjtjrl303 <gjtjrl303@naver.com>
2024-11-18 00:03:15 +0900

    delete : Friends 엔티티 및 FriendsRepository 삭제

--- src/main/java/com/example/chatapp/repository/FriendsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.chatapp.repository;
-
-import com.example.chatapp.domain.member.Friends;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FriendsRepository extends JpaRepository<Friends, Long> {
-
-}
